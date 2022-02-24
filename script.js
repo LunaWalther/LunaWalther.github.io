d59560c6dd9023da893533ce96913453e25f13d3
Yunus453 <maman.yunus@gmail.com>
2022-02-24 12:02:48 +0100

    m

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+alert("Willkommen bei Hangman");
+const buttonContainer= document.querySelector(`.buttonContainer´)
+buttonContainer.addEventListener(´click`,signalButton);
+
+
+
+
 function signalButton(){
-  alert("hallo");
+  alert("Hallo");
 }
