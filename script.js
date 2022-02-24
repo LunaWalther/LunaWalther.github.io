1c11453a5c886758eb6b2c07ca7c7b135062921d
Marco <marco.geiger@inovex.de>
2022-02-24 12:49:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 <!-- alert("Willkommen bei Hangman"); --> 
-const buttonContainer = document.querySelector(".button-container");
+const buttonContainer = document.querySelector('.buttonContainer');
 buttonContainer.addEventListener('click', signalButton);
 
 
 
 
-function signalButton(){
-  alert("Hallo");
+function signalButton(event){
+  <!-- damit bekommt man das auslösende Element, unseren <button...> -->
+  const button = event.target;
+  
+  <!--
+   if (button.nodeName !== 'BUTTON') {
+    return;
+  }
+  -->
+  
+  
+  <!-- damit bekommt man den Wert des Buttons <button..>WERT</button>
+  const letter = button.textContent;
+  
+  <!-- damit kann man den Button deaktivieren -->
+  <!-- button.disabled = true; -->
+  
 }
+
+
