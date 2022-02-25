c695a4b517444eecb73b51243285b7e005f0966a
LunaWalther <luna.walther9@gmail.com>
2022-02-25 12:04:05 +0100

    buchtaben

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-let counter = 0;
+let counter = 1;
 <!-- alert("Willkommen bei Hangman"); --> 
-const buttonContainer = document.querySelector('.buttonContainer');
-buttonContainer.addEventListener('click', signalButton);
+const buttonContainer = document.querySelectorAll('.buttonContainer');
+for (let i = 0; i < buttonContainer.length; i++) {
+	buttonContainer[i].addEventListener('click', signalButton);
+}
 
 
+const svg = document.querySelector('svg');
+
+document.getElementById("neustart").addEventListener('click', neustart);
+ function neustart(event){
+	 for (let i = 0; i < buttonContainer.length; i++) {
+	     for (let j = 1; j < buttonContainer[i].children.length; j++){
+			 buttonContainer[i].children[j].disabled = false;
+		 }
+	 }
+	 counter = 1
+     for (let i = 1; i < svg.children.length; i++) {
+          document.querySelector('svg').children[i].style.visibility = "hidden" ;
+		}
+
+ }
 
 
 function signalButton(event){
@@ -20,17 +37,21 @@ function signalButton(event){
   <!-- damit bekommt man den Wert des Buttons <button..>WERT</button> -->
   const letter = button.textContent;
   
-  alert("Der gedrückte Button war: " + letter);
+  
   
   <!-- damit kann man den Button deaktivieren -->
-  <!-- button.disabled = true; -->
-  counter = counter + 1;
+   button.disabled = true; 
   
+  
+  <!--
   if(counter < 2){
-     document.getElementById("Luna").style.visibility = "hidden";
+    document.getElementById("Luna").style.visibility = "visible";
   } else {
-    document.getElementById("Yunus").style.visibility = "hidden";
+    document.getElementById("Yunus").style.visibility = "visible";
   }
+  -->
+  document.querySelector('svg').children[counter].style.visibility = "visible"
+  counter++;  ;
 }
 
 
